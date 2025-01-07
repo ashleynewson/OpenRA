b89cc6692edcbaab05bf7183f48d4d58be34d2a7
Ashley Newson <ashleynewson@smartsim.org.uk>
2025-01-07 22:48:08 +0000

    Add experimental RA procedural map generator
    Add an experimental procedural map generator for the Red Alert mod,
    along with supporting code that may assist in the development of map
    generators for other mods.
    
    Map generation may be accessed as a tool in the Map Editor. This change
    does not presently introduce direct lobby options for generated maps.
    
    Features:
    
    - Terrain with land, water, beaches, cliffs, roads, debris, and trees.
    - Placement of mpspawns, neutral buildings, and resources.
    - Rotational and mirror symmetry options.
    - Various configurable parameters with presets.
    - Deterministic with configurable seed.
    - Performant.

--- OpenRA.Mods.Common/Widgets/Logic/Editor/MapToolsLogic.cs
+++ OpenRA.Mods.Common/Widgets/Logic/Editor/MapToolsLogic.cs
@@ -19,16 +19,20 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 	{
 		[FluentReference]
 		const string MarkerTiles = "label-tool-marker-tiles";
+		[FluentReference]
+		const string MapGenerator = "label-tool-map-generator";
 
 		enum MapTool
 		{
-			MarkerTiles
+			MarkerTiles,
+			MapGenerator
 		}
 
 		readonly DropDownButtonWidget toolsDropdown;
 		readonly Dictionary<MapTool, string> toolNames = new()
 		{
-			{ MapTool.MarkerTiles, MarkerTiles }
+			{ MapTool.MarkerTiles, MarkerTiles },
+			{ MapTool.MapGenerator, MapGenerator }
 		};
 
 		readonly Dictionary<MapTool, Widget> toolPanels = new();
@@ -42,10 +46,11 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 
 			var markerToolPanel = widget.Get("MARKER_TOOL_PANEL");
 			toolPanels.Add(MapTool.MarkerTiles, markerToolPanel);
+			var mapGeneratorToolPanel = widget.Get<ScrollPanelWidget>("MAP_GENERATOR_TOOL_PANEL");
+			toolPanels.Add(MapTool.MapGenerator, mapGeneratorToolPanel);
 
 			toolsDropdown.OnMouseDown = _ => ShowToolsDropDown(toolsDropdown);
 			toolsDropdown.GetText = () => FluentProvider.GetMessage(toolNames[selectedTool]);
-			toolsDropdown.Disabled = true; // TODO: Enable if new tools are added
 		}
 
 		void ShowToolsDropDown(DropDownButtonWidget dropdown)
@@ -61,7 +66,7 @@ namespace OpenRA.Mods.Common.Widgets.Logic
 				return item;
 			}
 
-			var options = new[] { MapTool.MarkerTiles };
+			var options = new[] { MapTool.MarkerTiles, MapTool.MapGenerator };
 			dropdown.ShowDropDown("LABEL_DROPDOWN_TEMPLATE", 150, options, SetupItem);
 		}
 
