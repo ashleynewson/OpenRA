73f9741f11d1593f543026a3cdeb27e6e58fe5ea
Ashley Newson <ashleynewson@smartsim.org.uk>
2025-08-18 21:44:16 +0100

    DO NOT MERGE

--- OpenRA.Mods.Common/Traits/World/TilingPathTool.cs
+++ OpenRA.Mods.Common/Traits/World/TilingPathTool.cs
@@ -357,7 +357,7 @@ namespace OpenRA.Mods.Common.Traits
 					.Where(multiBrush => multiBrush.Segment != null)
 					.ToImmutableArray();
 
-			IsEnabled = SegmentedBrushes.Length > 0;
+			IsEnabled = SegmentedBrushes.Length > 0 || IsDeveloper;
 			if (!IsEnabled)
 				return;
 
